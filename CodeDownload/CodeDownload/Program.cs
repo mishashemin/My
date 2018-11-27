5c6c102ea57d142a4cfd93d1d96b1721c25df10d
mishashemin <45352746+mishashemin@users.noreply.github.com>
2018-11-27 14:24:58 +0700

    Add files via upload

---
+++ CodeDownload/CodeDownload/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using Expression;
+
+namespace CodeDownload
+{
+    class Program
+    {
+        static Expr Main(string[] args)
+        {
+            var compiler = CodeDomProvider.CreateProvider("CSharp");
+            var compilerParams = new CompilerParameters()
+            {
+                CompilerOptions = "/t:library",
+                GenerateExecutable = true,
+                GenerateInMemory = false,
+            };
+            compilerParams.ReferencedAssemblies.Add("System.dll");
+            string source =
+            @"             
+             using System;
+             namespace Foo
+             {
+                public class Bar
+                {                  
+                   public static void SayHello()
+                   {
+                      System.Console.WriteLine(""Hello World"");
+                   }
+               }
+            }
+            ";
+            CompilerResults res = compiler.CompileAssemblyFromSource(compilerParams, source);
+            res.CompiledAssembly.GetType("Foo.Bar").GetMethod("SayHello").Invoke(null, null);
+            Console.ReadKey();
+            return new Constant(0);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expression;
+using ExprParsing;
+
+namespace ConsoleApp2
+{
+    using static Fun;   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain domain = AppDomain.CreateDomain("MyDomain");
+            InterfBase cr = (InterfBase)domain.CreateInstanceFromAndUnwrap("ExprParsing.dll", "ExprParsing.Class1");
+            cr.SayHello();
+            
+            
+            var a = new Variable("a");
+            var b = new Variable("b");
+            var c = Pow(2, 4);
+            var ss = c.Diff();
+            double vv = c.Compute();
+            vv = ss.Compute(new Dictionary<string, double>
+            {
+                ["a"] = 4,
+                ["b"] = 30
+            });
+            vv = c.Compute(new Dictionary<string, double>
+            {
+                ["a"] = 4,
+                ["b"] = 3
+            });
+        }
+    }
+}
